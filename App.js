a2d79839b773ee494ee4eef0fd726d71c0f1a1e8
wooseung <shdntmd44@naver.com>
2020-08-04 19:22:46 +0900

    completed

--- App.js
+++ App.js
@@ -3,18 +3,25 @@ import Loading from "./Loading";
 import { Alert } from "react-native";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 const API_KEY = "9d2dc547abdd90969255612b5761cc82";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+    temp:0,
+    condition:"Clear"
   };
   getWeather = async (lat, lon) => {
-    console.log(lat, lon)
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
+    this.setState({ isLoading: false, temp, condition: weather[0].main });
   };
   getLocation = async () => {
     try {
@@ -22,7 +29,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      console.log(latitude)
+      console.log(latitude);
       this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (e) {
@@ -33,7 +40,11 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
---
+++ Weather.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { StyleSheet, View, Text, StatusBar } from "react-native";
+import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#232526", "#414345"],
+  },
+  Drizzle: {
+    iconName: "weather-rainy",
+    gradient: ["#00416A", "#E4E5E6"],
+  },
+  Rain: {
+    iconName: "weather-pouring",
+    gradient: ["#00416A", "#E4E5E6"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#C9D6FF", "#E2E2E2"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#f8b500", "#fceabb"],
+  },
+  Cloudes: {
+    iconName: "weather-cloudy",
+    gradient: ["#232526", "#414345"],
+  },
+  Haze: {
+    iconName: "weather-hazy",
+    gradient: ["#757F9A", "#D7DDE8"],
+  },
+  Mist: {
+    iconName: "weather-fog",
+    gradient: ["#403B4A", "#E7E9BB"],
+  },
+  Smoke: {
+    iconName: "weather-fog",
+    gradient: ["#403B4A", "#E7E9BB"],
+  },
+  Dust: {
+    iconName: "weather-fog",
+    gradient: ["#403B4A", "#E7E9BB"],
+  },
+  Fog: {
+    iconName: "weather-fog",
+    gradient: ["#403B4A", "#E7E9BB"],
+  },
+  Sand: {
+    iconName: "weather-fog",
+    gradient: ["#403B4A", "#E7E9BB"],
+  },
+  Squall: {
+    iconName: "weather-pouring",
+    gradient: ["#bdc3c7", "#2c3e50"],
+  },
+  Tornado: {
+    iconName: "weather-tornado",
+    gradient: ["#bdc3c7", "#2c3e50"],
+  },
+};
+
+export default function Weather({ temp, condition }) {
+  console.log(condition);
+  return (
+    <LinearGradient
+      // Background Linear Gradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
+        <MaterialCommunityIcons
+          name={weatherOptions[condition].iconName}
+          size={96}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}º</Text>
+    </LinearGradient>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Cloudes",
+    "Haze",
+    "Mist",
+    "Smoke",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado",
+  ]).isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  harfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42,
+    color: "white",
+  },
+});
